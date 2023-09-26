b6252af3e7e2bb729663aa1cca734331ee471c76
TALYSLAN CAUAN PIMENTEL CANABARRO <2022317597@IFAL-MACEIO.INTRANET>
2023-09-26 10:40:46 -0300

    order changed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const number = document.querySelector("span");
-const plus = document.querySelectorAll("button")[0];
-const minus = document.querySelectorAll("button")[1];
+const plus = document.querySelectorAll("button")[1];
+const minus = document.querySelectorAll("button")[0];
 
 const plusNumber = () => number.innerHTML = 1 + parseInt(number.innerHTML);
 const minusNumber = () => number.innerHTML = parseInt(number.innerHTML) - 1;
